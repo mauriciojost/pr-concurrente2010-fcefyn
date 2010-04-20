ddafbf0676b383dd6952c0ecfc2bae64d9b77bbf
bazanfedericoa <bazanfedericoa@ae2b51aa-fa15-f9c4-29eb-c7ba638bb5f5>
2010-04-20 22:25:09 +0000

    Carrera. Base para segundo práctico.

---
+++ J2SE/proyectos/carrera/src/paquete/Corredor.java
@@ -0,0 +1,6 @@
+package paquete;
+
+public interface Corredor extends Runnable {
+
+	public void correr();
+}
---
+++ J2SE/proyectos/carrera/src/paquete/Gato.java
@@ -0,0 +1,22 @@
+package paquete;
+
+public class Gato extends Animal implements Corredor{
+
+	Gato(String nombre) {
+		super(nombre);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void correr() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
