a7ef65ad69c40dd5a90253a1aded0190169d2ab3
mauriciojost <mauriciojost@ae2b51aa-fa15-f9c4-29eb-c7ba638bb5f5>
2010-04-21 20:14:10 +0000

    Preparando la competencia...

--- trunk/J2SE/proyectos/cracker/paquete/Main.java
+++ trunk/J2SE/proyectos/cracker/paquete/Main.java
@@ -9,7 +9,24 @@ public class Main {
 		//Cracker miCracker 	= new MiCracker();
 		
 		caja.addCracker(cracker_seq);
-		//caja.addCracker(miCracker);
+		caja.addCracker(new paquete.Cracker_200204474());
+		caja.addCracker(new paquete.Cracker_200204946());
+		caja.addCracker(new paquete.Cracker_200404117());
+		caja.addCracker(new paquete.Cracker_30353812());
+		caja.addCracker(new paquete.Cracker_30995308());
+		caja.addCracker(new paquete.Cracker_32187957());
+		caja.addCracker(new paquete.Cracker_32578216());
+		caja.addCracker(new paquete.Cracker_32943329());
+		caja.addCracker(new paquete.Cracker_33242359());
+		caja.addCracker(new paquete.Cracker_33303393());
+		caja.addCracker(new paquete.Cracker_33320188());
+		caja.addCracker(new paquete.Cracker_33700254());
+		caja.addCracker(new paquete.Cracker_33870228());
+		caja.addCracker(new paquete.Cracker_33941256());
+		caja.addCracker(new paquete.cracker_33959924());
+		caja.addCracker(new paquete.Cracker_34189197());
+		caja.addCracker(new paquete.Cracker_34346712());
+		caja.addCracker(new paquete.Cracker_34441188());
 		
 		caja.testCrackers(10000);
 	}
