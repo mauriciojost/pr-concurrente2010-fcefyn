4decb63ea029870827b5c31bc13e5e6e3bfbaa02
bazanfedericoa <bazanfedericoa@ae2b51aa-fa15-f9c4-29eb-c7ba638bb5f5>
2010-04-23 21:25:28 +0000

    Carrera. Mejorando Pista.

--- J2SE/proyectos/carrera/src/paquete/Pista.java
+++ J2SE/proyectos/carrera/src/paquete/Pista.java
@@ -1,16 +1,16 @@
 package paquete;
 
+import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Vector;
 
 /* Clase que contiene varios corredores, y la información asociada a la posición de cada uno. */
 public class Pista {
 	
-	private Vector<Corredor> corredores; /* Colección de corredores agregados a la pista. */
+	private HashMap<Corredor, Integer> corredores; /* Colección de corredores agregados a la pista. */
 	
 	/* Nueva pista. */
 	public Pista(){
-		corredores = new Vector<Corredor>();
+		corredores = new HashMap<Corredor, Integer>();
 	}
 	
 	/* Cambio de posición de un corredor en particular. */
@@ -18,20 +18,22 @@ public class Pista {
 		
 		//Algoritmo para decidir si es penalizado o avanza.
 		Integer valor = Integer.parseInt(position, 16);
-		String str = Integer.toString(valor.intValue()+1, 16);
-		System.out.println("El corredor " + c.getID() + " se encuentra en: " + position);
-		return str;
+		if (corredores.get(c)+1 != valor)
+			valor = 0;	// Penalizado. Por vivo!
+		
+		corredores.put(c, valor);	// Actualización de la posición
+		return Integer.toString(valor.intValue(), 16);
 	}
 	
 	/* Agregado de corredores. */
 	public void addCorredor(Corredor corredor){
-		corredores.add(corredor);
+		corredores.put(corredor, 0);
 		corredor.setPista(this);
 	}
 	
 	/* Puesta en marcha de cada uno de los corredores. */
 	public void iniciarCarrera(){
-		Iterator<Corredor> it = corredores.iterator();
+		Iterator<Corredor> it = corredores.keySet().iterator();
 		Corredor actual;
 		while(it.hasNext()){
 			actual = (Corredor)it.next();
