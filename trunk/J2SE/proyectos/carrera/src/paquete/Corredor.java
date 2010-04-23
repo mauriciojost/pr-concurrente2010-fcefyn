224bd5d6e65a15f25c60536797ede6a67cba159a
mauriciojost <mauriciojost@ae2b51aa-fa15-f9c4-29eb-c7ba638bb5f5>
2010-04-23 20:07:43 +0000

    Actualizado el proyecto.

--- trunk/J2SE/proyectos/carrera/src/paquete/Corredor.java
+++ trunk/J2SE/proyectos/carrera/src/paquete/Corredor.java
@@ -1,6 +1,7 @@
 package paquete;
 
 public interface Corredor extends Runnable {
-
 	public void correr();
+	public void setPista(Pista pista);
+	public String getID();
 }
--- trunk/J2SE/proyectos/carrera/src/paquete/Pista.java
+++ trunk/J2SE/proyectos/carrera/src/paquete/Pista.java
@@ -1,16 +1,38 @@
 package paquete;
 
+import java.util.Iterator;
 import java.util.Vector;
 
 public class Pista {
 	
 	private Vector<Corredor> corredores;
 	
-	public int setPosition(Corredor c, int position){
+	public Pista(){
+		corredores = new Vector<Corredor>();
+	}
+	
+	public String setPosition(Corredor c, String position){
 		
 		//Algoritmo para decidir si es penalizado o avanza.
+		System.out.println("El corredor " + c.getID() + " se encuentra en: " + position);
+		Integer valor = Integer.parseInt(position, 16);
+		String str = Integer.toString(valor.intValue()+1, 16);
+		return str;
 		
-		return position;
+	}
+	
+	public void addCorredor(Corredor corredor){
+		corredores.add(corredor);
+		corredor.setPista(this);
+	}
+	
+	public void iniciarCarrera(){
+		Iterator it = corredores.iterator();
+		Corredor actual;
+		while(it.hasNext()){
+			actual = (Corredor)it.next();
+			actual.correr();
+		}
 	}
 
 }
