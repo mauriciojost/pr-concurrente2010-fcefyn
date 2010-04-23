bccefbdc89e01142797ed494ad4320cfee8b8935
mauriciojost <mauriciojost@ae2b51aa-fa15-f9c4-29eb-c7ba638bb5f5>
2010-04-23 21:21:51 +0000

    Gato picarón que salta a veces de a dos posiciones. Perro que avanza (antes no avanzaba!). Algunos competidores en la pista.

--- J2SE/proyectos/carrera/src/paquete/Gato.java
+++ J2SE/proyectos/carrera/src/paquete/Gato.java
@@ -1,33 +1,48 @@
 package paquete;
 
-public class Gato extends Animal implements Corredor{
+import java.util.Random;
 
+/* Clase Perro. <Es> Animal y <puede> Correr. */
+public class Gato extends Animal implements Corredor{
+	private Pista pista; /* Como Corredor necesita conocer la Pista para actualizar su posición al correr. */
+	private int position=0; /* Posición actual del Gato. */
+	
 	Gato(String nombre) {
-		super(nombre);
-		// TODO Auto-generated constructor stub
+		super(nombre); /* Constructor de la super-clase (Animal). */
 	}
 
 	@Override
 	public void correr() {
-		// TODO Auto-generated method stub
+		/* A los fines didácticos se desglosa esta parte. */
+		Runnable runab = this; 
+		String nombre_hilo = this.getID();
+		Thread nuevo_hilo = (new Thread(runab,nombre_hilo)); /* Creación de un nuevo hilo. */
+		nuevo_hilo.start();
 	}
-
+	
+	
+	/* Método que será ejecutado desde un hilo independiente al hacer 'hilo.start()'. */
+	/* Este gato traicionero quiere avanzar de a 2 algunas veces. */
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		Random rnd = new Random();
+		int step;
+		while(true){
+			step = ((rnd.nextFloat()>0.9)?2:1);
+			position = Integer.parseInt(pista.setPosition(this, Integer.toHexString(position+step)),16);
+		}
 	}
 
-	@Override
-	public String getID() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	/* Establece la pista sobre la cual este corredor realizará actualizaciones al correr. */
 	@Override
 	public void setPista(Pista pista) {
-		// TODO Auto-generated method stub
-		
+		this.pista = pista;
+	}
+
+	/* Retornar el nombre del Alumno! */
+	@Override
+	public String getID() {
+		return this.getNombre();
 	}
 
 }
--- J2SE/proyectos/carrera/src/paquete/Main.java
+++ J2SE/proyectos/carrera/src/paquete/Main.java
@@ -3,9 +3,16 @@ package paquete;
 public class Main {
 	public static void main(String[] args) {
 		Pista pista;
+		
 		pista = new Pista();
-		Corredor pichu = new Perro("pichu");
+		Corredor pichu = new Perro("Pichu");
+		Corredor roque = new Perro("Roque");
+		Corredor michi = new Gato ("Michi");
+		
 		pista.addCorredor(pichu);
+		pista.addCorredor(roque);
+		pista.addCorredor(michi);
+		
 		pista.iniciarCarrera();
 	}
 
