ba049efc8047c752f8b23ab32db769b6ae29873f
renzopisetta <renzopisetta@ae2b51aa-fa15-f9c4-29eb-c7ba638bb5f5>
2010-04-24 17:19:53 +0000

    Agredado corredor Perezoso a la carrera.

--- J2SE/proyectos/carrera/src/paquete/Main.java
+++ J2SE/proyectos/carrera/src/paquete/Main.java
@@ -9,11 +9,13 @@ public class Main {
 		Corredor roque = new Perro("Roque");
 		Corredor picho = new PerroPicaron ("Picho");
 		Corredor lapradon = new Gato("Michi");
+		Corredor zzz=new Perezoso("zzz");
 		
 		pista.addCorredor(pichu);
 		pista.addCorredor(roque);
 		pista.addCorredor(picho);
 		pista.addCorredor(lapradon);
+		pista.addCorredor(zzz);
 		
 		pista.iniciarCarrera();
 	}
