9b2292da126809342129915cf1455d30df64b916
luisgustavorodriguez <luisgustavorodriguez@ae2b51aa-fa15-f9c4-29eb-c7ba638bb5f5>
2010-04-26 00:03:14 +0000

    Agrego mi clase de corredor Tigre

---
+++ J2SE/proyectos/carrera/src/paquete/Tigre.java
@@ -0,0 +1,33 @@
+package paquete;
+
+public class Tigre extends Animal implements Corredor {
+	
+	public Tigre (String tigre){
+		super(tigre);
+	}
+
+	@Override
+	public void correr() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setPista(Pista pista) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
