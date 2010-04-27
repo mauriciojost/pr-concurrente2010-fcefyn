bfb91d7205a00b6f2627e20187f0a8023f70abdc
diario292 <diario292@ae2b51aa-fa15-f9c4-29eb-c7ba638bb5f5>
2010-04-27 01:15:46 +0000

    Agregue mi corredor: Sonic. "Acomode" un poco el codigo del main.

--- J2SE/proyectos/carrera/src/paquete/Main.java
+++ J2SE/proyectos/carrera/src/paquete/Main.java
@@ -2,22 +2,23 @@ package paquete;
 
 public class Main {
 	public static void main(String[] args) {
+		
 		Pista pista;
-		
 		pista = new Pista();
-		Corredor pichu = new Perro("Pichu de Federico Bazán");
-		Corredor roque = new Perro("Roque de Mauricio Jost");
 		
-		Corredor dormilon=new Perezoso("Dormilon de Renzo Pisseta");		
-		Corredor chita= new Chita ("Osita de Melisa Torres");
-		//Corredor ranita=new Rana("Rene de Alejandro Rueda");		
+		Corredor pichu 	= 	new Perro("Pichu de Federico Bazán");
+		Corredor roque 	= 	new Perro("Roque de Mauricio Jost");
+		
+		Corredor dormilon =	new Perezoso("Dormilon de Renzo Pisseta");		
+		Corredor chita = 	new Chita ("Osita de Melisa Torres");
+		//Corredor ranita =	new Rana("Rene de Alejandro Rueda");		
 		Corredor condorito = new Buitre("Condorito");
 		Corredor kintaafondo = new Gacela("ComoPinia");		
-		Corredor tigre =new Tigre("Tigre de Luis Rodriguez");
+		Corredor tigre =	new Tigre("Tigre de Luis Rodriguez");
 		Corredor cangrejo = new Cangrejo("Bicho de Dario Samban");
-		Corredor coyote = new Coyote("Acme");
+		Corredor coyote = 	new Coyote("Acme");
 		Corredor Bernardo = new Dinosaurio("El Dinosaurio Bernardo");
-		
+		Corredor sonic = 	new Puercoespin("Sonic de Nico Morales");
 		
 		
 		pista.addCorredor(Bernardo);
@@ -31,6 +32,7 @@ public class Main {
 		pista.addCorredor(kintaafondo);
 		pista.addCorredor(tigre);
 		pista.addCorredor(coyote);
+		pista.addCorredor(sonic);
 		pista.iniciarCarrera();
 	}
 
