4f7fb6c59a32d6cac9b3033355a680199106aa8e
mflorenciacaro88 <mflorenciacaro88@ae2b51aa-fa15-f9c4-29eb-c7ba638bb5f5>
2010-04-27 15:48:12 +0000

    Falta completar

--- J2SE/proyectos/carrera/src/paquete/Pantera.java
+++ J2SE/proyectos/carrera/src/paquete/Pantera.java
@@ -1,5 +1,35 @@
 package paquete;
 
-public class Pantera {
+public class Pantera extends Animal implements Corredor
+{
+	protected Pista pista; 
+	protected int position=0;
 
+	Pantera (String nombre) 
+	{
+		super(nombre);
+		
+	}
+	
+	
+	public void correr() 
+    {
+		//Implementar creacion del hilo para correr
+	}
+	
+	public String getID() 
+	{
+		return getNombre();
+	}
+	
+	public void setPista(Pista pista) 
+	{
+		this.pista = pista;
+	}
+	
+	public void run() 
+    {
+		//Implementar como corre el animal
+	}
+	
 }
