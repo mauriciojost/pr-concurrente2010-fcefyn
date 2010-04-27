f64002fa2f4ff5cc3bdbf011948be1742eb36db6
ernesto.aoc <ernesto.aoc@ae2b51aa-fa15-f9c4-29eb-c7ba638bb5f5>
2010-04-27 17:28:44 +0000

    Corredor Terminado: Pollo

---
+++ trunk/J2SE/proyectos/carrera/src/paquete/Pollo.java
@@ -0,0 +1,48 @@
+package paquete;
+
+public class Pollo extends Animal implements Corredor {
+	protected Pista pista; 
+	protected int posicion=0;
+		
+	Pollo(String nombre) {
+		super(nombre);
+		
+	}
+
+	public void correr() {
+		
+		
+		Runnable runab = this; 
+		String nombre_hilo = this.getID();
+		Thread pollo_hilo = (new Thread(runab,nombre_hilo));
+		pollo_hilo.setPriority(10);
+		pollo_hilo.start();
+		
+	}
+
+public String getID() {
+
+return this.getNombre();
+}
+
+public void setPista(Pista pista) {
+
+this.pista=pista;
+}
+
+public void run() {
+
+try{
+	while(true){
+		
+		posicion = Integer.parseInt(pista.setPosition(this, Integer.toHexString(posicion+1)),10);
+	}
+}catch(NumberFormatException e){
+	System.out.println("Soy " + this.getID() +". Llegue al final! <<<<<<<<<<<<<<<");
+}
+}
+
+
+}
+
+
