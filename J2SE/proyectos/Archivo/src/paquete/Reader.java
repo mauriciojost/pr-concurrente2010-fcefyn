26da3517807b3a22da23e35368e1313c593d87e2
bazanfedericoa <bazanfedericoa@ae2b51aa-fa15-f9c4-29eb-c7ba638bb5f5>
2010-05-12 12:56:06 +0000

    Practico Buffer/Archivo. En la página está presentado. Allí se indica que deben hacer.

---
+++ J2SE/proyectos/Archivo/src/paquete/Reader.java
@@ -0,0 +1,15 @@
+package paquete;
+
+public class Reader implements Runnable {
+	private Buffer b;
+	
+	public Reader(Buffer b){
+		this.b = b;
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ J2SE/proyectos/Archivo/src/paquete/Writer.java
@@ -0,0 +1,16 @@
+package paquete;
+
+public class Writer implements Runnable{
+	private Buffer b;
+	
+	public Writer(Buffer b){
+		this.b = b;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
