4ba31bb62e0ad0c676639136f5605e325ce74ed9
mauriciojost <mauriciojost@ae2b51aa-fa15-f9c4-29eb-c7ba638bb5f5>
2010-06-09 00:31:58 +0000

    Cleaning up the generator project (Main class).

--- J2SE/proyectos/generator/src/main/Main.java
+++ J2SE/proyectos/generator/src/main/Main.java
@@ -1,10 +1,9 @@
 package main;
 
+/* Agregue su paquete aqui. */
 import mauricio.*;
 import ortizsoler.*;
-
-
-import LuisRodriguez.MiMain;
+import LuisRodriguez.*;
 
 
 
@@ -16,7 +15,7 @@ public class Main {
 	public static final int MATRICULA_MAURICIO = 200404067;
 	public static final int MATRICULA_ORTIZ = 33320188;
 	public static final int MATRICULA_SOLER = 33843410;
-	public static final int MATRICULA_LUIS_RODRIGUEZ=32187957;
+	public static final int MATRICULA_LUIS_RODRIGUEZ = 32187957;
 	
 	/* Fin de matrículas. */
 	
@@ -25,29 +24,34 @@ public class Main {
 		int alumno=MATRICULA_LUIS_RODRIGUEZ;
 		
 		switch(alumno){
+		
 			/* Ingresar el bloque de código de inicialización para cada alumno. */
+		
 			case MATRICULA_MAURICIO: 
 
-		/*		Generator generator = new Generator(N_CHANNELS,1);
-				generator.run();
+				//Generator generator = new Generator(N_CHANNELS,1);
+				//generator.run();
 
 				Generator generator = new Generator(N_CHANNELS, 1);
 				new OneFile200404067(generator);
 
-				new Otra(generator);*/
+				new Otra(generator);
 				break;
 			case MATRICULA_ORTIZ:
+				/*
 				new Pantalla();
 				break;
+				*/
 			case MATRICULA_SOLER:
+				/*
 				new Pantalla();
 				break;
+				*/
 			case MATRICULA_LUIS_RODRIGUEZ:
+				/*
 				MiMain mm=new MiMain(); //inicializacion de la pantalla y otros objetos
-				
 				break;	
-				
-				
+				*/
 				
 			default:
 				System.out.println("Ingrese su nro. de alumno e implemente su sección de código.");
