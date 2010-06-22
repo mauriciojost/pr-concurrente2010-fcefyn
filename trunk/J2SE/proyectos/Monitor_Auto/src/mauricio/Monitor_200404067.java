63311475084026958d2fda891d20687e0be008b9
mauriciojost <mauriciojost@ae2b51aa-fa15-f9c4-29eb-c7ba638bb5f5>
2010-06-22 03:08:22 +0000

    Actualizando práctico.

---
+++ trunk/J2SE/proyectos/Monitor_Auto/src/mauricio/Monitor_200404067.java
@@ -0,0 +1,31 @@
+package mauricio;
+
+import paquete.Pasajero;
+import paquete.Vehiculo;
+
+public class Monitor_200404067 extends Vehiculo {
+
+	public Monitor_200404067(int ingresos_por_a, int ingresos_por_b) {
+		super(0, 0); /* Atributos no utilizados (se utilizarán los del vehículo a monitorear). */
+		
+	}
+
+	@Override
+	public void dejarEntrarAPasajeroPorPuertaA(Pasajero p) {
+
+		
+	}
+
+	@Override
+	public void dejarEntrarAPasajeroPorPuertaB(Pasajero p) {
+
+		
+	}
+
+	@Override
+	public String getAPELLIDONombreDelAlumno() {
+		
+		return "JOST, Mauricio G.";
+	}
+
+}
